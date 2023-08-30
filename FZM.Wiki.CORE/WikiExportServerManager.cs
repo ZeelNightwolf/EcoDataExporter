d67f56bdd0dfc6dc14aad7d8d212f6a35f4982e9
VeN001 <143325226+VeN044@users.noreply.github.com>
2023-08-30 08:44:11 +0300

    ServerGUI fix
    Add commsnds to ServerGUI

--- FZM.Wiki.CORE/WikiExportServerManager.cs
+++ FZM.Wiki.CORE/WikiExportServerManager.cs
@@ -6,13 +6,16 @@ using System.Collections.Generic;
 namespace FZM.Wiki
 {
     [LocDisplayName("FZM Wiki Export")]
-    public partial class WikiDetails
-    {  
+    public class WikiExportPlugin : IModKitPlugin, ICommandablePlugin
+    {
         public void GetCommands(Dictionary<string, Action> nameToFunction)
         {
-            nameToFunction.Add(Localizer.DoStr("Dump Details"), () => DumpDetails(null));
+            nameToFunction.Add((string)Localizer.DoStr("Export All Data"), new Action(() => WikiExportCommandHandler.WikiExportCommands.dumpdetails(null)));
         }
+        string status = "Loaded.";
+        public override string ToString() => (string)Localizer.DoStr("FZM Wiki");
 
-        public string GetStatus() => "Eco Data Extrator Active.";
+        public string GetStatus() => this.status;
+        public string GetCategory() => "Mods";
     }
 }
